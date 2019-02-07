c2516aa0de2e3b531b4b3ced2ae2279c15f55adf
anilyadav0489 <anilyadav0489@gmail.com>
2019-02-08 00:44:05 +0530

    Added logic for different names

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import heart from './heart.png';
 
 class App extends Component {
+  name = 'Angel'
+  constructor(props){
+    super(props);
+    this.name = window.location.href;
+    this.name = this.name.split('/');
+    this.name = this.name.slice(-1)+"";
+  }
+  componentWillUpdate=()=>{
+    
+  }
   render() {
+    let nameToDisplay = "Angel"
+    if(this.name == 'yx2pn77b'){
+      nameToDisplay = "Anjali"
+    } else if(this.name == 'lk4jovfd9'){
+      nameToDisplay = 'Maggi'
+    }
     return (
       <div className="App">
         <div className="stars"></div>
         <div className="twinkling heart-div">
           <img src={heart} className="heart"></img>
           <label id="pre-text">I love you my</label>
-          <label id="name" className="pulse">Angel</label>
+          <label id="name" className="pulse">{nameToDisplay}</label>
         </div>
         <div className="clouds"></div>
         <div id="author">Created by Anil Yadav</div>
