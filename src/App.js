19ba48b90d868d1eae38309c72cd78d22f9c14a0
anilyadav0489 <anilyadav0489@gmail.com>
2019-02-08 00:26:13 +0530

    Added heart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import heart from './heart.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="stars"></div>
-        <div class="twinkling"></div>
-        <div class="clouds"></div>
+        <div className="stars"></div>
+        <div className="twinkling heart-div">
+          <img src={heart} className="heart"></img>
+          <label id="pre-text">I love you my</label>
+          <label id="name" className="pulse">Angel</label>
+        </div>
+        <div className="clouds"></div>
+        <div id="author">Created by Anil Yadav</div>
       </div>
     );
   }
